e07b2ee6b7a8572a181f5d528fd00947c4cc208c
Anasfaiq <176678988+Anasfaiq@users.noreply.github.com>
2026-05-03 14:06:38 +0700

    feat: Adding dark mode

---
+++ src/components/DarkMode.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const DarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
+    const storedTheme = localStorage.getItem("theme");
+    const systemPrefersDark = window.matchMedia(
+      "prefers-color-scheme: dark",
+    ).matches;
+    return storedTheme === "dark" || (!storedTheme && systemPrefersDark);
+  });
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => {
+      const newTheme = !prev;
+      localStorage.setItem("theme", newTheme ? "dark" : "light");
+      return newTheme;
+    });
+  };
+
+  return {isDarkMode, toggleTheme};
+};
+
+export default DarkMode;
