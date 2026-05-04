a02083bec93f21ab40a17b85de18e0b3e49d90eb
Anasfaiq <176678988+Anasfaiq@users.noreply.github.com>
2026-05-04 12:31:43 +0700

    adding Pause icon

---
+++ src/components/ui/Pause.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  size: number;
+}
+
+const Pause = ({ size }: Props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-player-pause"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M6 6a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -12" />
+      <path d="M14 6a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -12" />
+    </svg>
+  );
+};
+
+export default Pause;
