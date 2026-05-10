8b7363fb73e62b0fdd0441b78849c9714f0837e7
Anasfaiq <176678988+Anasfaiq@users.noreply.github.com>
2026-05-10 09:59:59 +0700

    adding sticky to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,7 +15,7 @@ const NavBar = () => {
   });
 
   return (
-    <div className="flex items-center justify-between px-5 py-4 bg-(--lf-card)">
+    <div className="sticky top-0 z-50 flex items-center justify-between px-5 py-4 bg-(--lf-card) border-b border-(--lf-border)">
       <div className="flex gap-2 items-center font-medium text-xl">
         <span className="bg-(--lf-accent) text-white p-2 rounded-lg">
           <Clock />
@@ -23,7 +23,7 @@ const NavBar = () => {
         <span className="text-(--lf-primary)">LofiFocus</span>
       </div>
       <div className="flex gap-3 items-center">
-        <p className="text-(--lf-accent)">{formatted}</p>
+        <p className="text-(--lf-accent) text-sm truncate">{formatted}</p>
         <button
           onClick={toggleTheme}
           className="px-3 py-1 bg-(--lf-bg) text-(--lf-accent) text-sm rounded-lg"
