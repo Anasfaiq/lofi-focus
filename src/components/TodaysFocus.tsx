776030c9e0395d31106d756181914bb5af2e94d9
Anasfaiq <176678988+Anasfaiq@users.noreply.github.com>
2026-05-10 09:37:59 +0700

    adding TodaysFocus component

---
+++ src/components/TodaysFocus.tsx
@@ -0,0 +1,41 @@
+type Stats = {
+  totalSessions: number;
+  focusMinutes: number;
+  streak: number;
+};
+
+type Props = {
+  stats: Stats;
+};
+
+const TodaysFocus = ({ stats }: Props) => {
+  const focusHrs = (stats.focusMinutes / 60).toFixed(1);
+
+  return (
+    <div className="bg-(--lf-card) p-6 rounded-2xl flex flex-col justify-center gap-3 w-full">
+      <div>
+        <p>Today's Focus</p>
+      </div>
+      <div className="bg-(--lf-bg) border border-(--lf-border) px-4 py-3 rounded-xl text-sm flex items-center justify-between gap-2">
+        <p className="text-(--lf-accent) font-medium">Focus hrs</p>
+        <p className="text-(--lf-accent) text-lg font-medium whitespace-nowrap">
+          {focusHrs} <span className="text-xs font-medium">h</span>
+        </p>
+      </div>
+      <div className="bg-(--lf-bg) border border-(--lf-border) px-4 py-3 rounded-xl text-sm flex items-center justify-between gap-2">
+        <p className="text-(--lf-accent) font-medium">Streak</p>
+        <p className="text-(--lf-accent) text-lg font-medium whitespace-nowrap">
+          {stats.streak} <span className="text-xs font-medium">d</span>
+        </p>
+      </div>
+      <div className="bg-(--lf-bg) border border-(--lf-border) px-4 py-3 rounded-xl text-sm flex items-center justify-between gap-2">
+        <p className="text-(--lf-accent) font-medium">Sessions</p>
+        <p className="text-(--lf-accent) text-lg font-medium whitespace-nowrap">
+          {stats.totalSessions}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TodaysFocus;
